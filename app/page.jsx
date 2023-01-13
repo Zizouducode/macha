04a6788d28aa68a7d912b14528f840e2fb93b19f
Karim Hamga <hamga.karim@gmail.com>
2023-01-13 18:37:45 +0100

    response

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ async function getData() {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`
     );
+    console.log("response>>>", response);
     return response.json();
   } catch (error) {
     console.log(error.message);
